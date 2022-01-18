fd194c5c103c3c5381995ca89fc690a6758b13b7
Kartheek Kadaru <Kartheek.Kadaru@gov.bc.ca>
2022-01-18 13:44:51 -0800

    worked on create application form and responded form

--- src/frontend/csrs-portal/src/app/shared/shared.module.ts
+++ src/frontend/csrs-portal/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { ConfigModule } from 'app/config/config.module';
@@ -10,6 +11,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { PageComponent } from './components/page/page.component';
+import { ConfirmDialogComponent } from './dialogs/confirm-dialog/confirm-dialog.component';
 import { NgxBusyModule } from './modules/ngx-busy/ngx-busy.module';
 import { NgxMaterialModule } from './modules/ngx-material/ngx-material.module';
 import { NgxProgressModule } from './modules/ngx-progress/ngx-progress.module';
@@ -35,6 +37,7 @@ import { YesNoPipe } from './pipes/yes-no.pipe';
     AlertComponent,
     HeaderComponent,
     FooterComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     CommonModule,
@@ -46,6 +49,7 @@ import { YesNoPipe } from './pipes/yes-no.pipe';
     NgxProgressModule,
     ConfigModule,
     TranslateModule,
+    MatDialogModule,
   ],
   exports: [
     CommonModule,
@@ -69,6 +73,7 @@ import { YesNoPipe } from './pipes/yes-no.pipe';
     HeaderComponent,
     FooterComponent,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   entryComponents: [],
 })
 export class SharedModule { }
