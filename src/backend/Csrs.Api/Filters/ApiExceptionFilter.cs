3eb0a0a41df330b9912f77f621b89c3a70f6af30
TayGov <taylor.clausen@gov.bc.ca>
2022-02-17 13:04:07 -0800

    Initial

--- src/backend/Csrs.Api/Filters/ApiExceptionFilter.cs
+++ src/backend/Csrs.Api/Filters/ApiExceptionFilter.cs
@@ -1,4 +1,5 @@
-﻿using Csrs.Interfaces.Dynamics;
+﻿using Csrs.Api.Models;
+using Csrs.Interfaces.Dynamics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -6,24 +7,36 @@ namespace Csrs.Api.Filters
 {
     public class ApiExceptionFilter : ExceptionFilterAttribute
     {
+
+        private readonly ILogger<ApiExceptionFilter> _logger;
+
+        public ApiExceptionFilter(ILogger<ApiExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
         public override void OnException(ExceptionContext context)
         {
 
-            string response;
+            ApiError response = new ApiError();
 
             if (context.Exception is InvalidIdException)
             {
 
-                var ex = context.Exception as InvalidIdException;
-                context.Exception = null;
-                response = ex.Message;
+                var ex = context.Exception as InvalidIdException;     
+                response.Error = "Invalid Id";
+                response.Message = ex.Message;
 
+                context.Exception = null;
                 context.HttpContext.Response.StatusCode = 400;
+
             }
             else
             {
+
                 context.HttpContext.Response.StatusCode = 500;
-                response = context.Exception.Message;
+                response.Error = context.Exception.GetType().Name;
+                response.Message = context.Exception.Message;
+
             }
 
             context.Result = new JsonResult(response);
