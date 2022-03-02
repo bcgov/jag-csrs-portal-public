c6ca5a2482d1ef292eeb55bacb3b467c2a891802
KKAYFISH <kyle.kayfish@gmail.com>
2022-03-02 11:59:33 -0800

    Removed creation of additional task

--- src/backend/Csrs.Api/Features/UserRequests/Create.cs
+++ src/backend/Csrs.Api/Features/UserRequests/Create.cs
@@ -120,9 +120,6 @@ namespace Csrs.Api.Features.UserRequests
                 task.Statuscode = 2; // Not Started
                 //ap.Statecode = 0;  defaults in DB
                 MicrosoftDynamicsCRMtask result = await _dynamicsClient.Tasks.CreateAsync(task);
-                string subject = "Contact Us Record Created";
-                string description = "User created a contact request for file: " + request.FileNo;
-                await _taskService.CreateTask(request.FileId, subject, description, cancellationToken);
                 _logger.AddProperty("ActivityId", result.Activityid);
                 _logger.LogDebug("User Request created successfully");
                 return new Response("User Request Created");
