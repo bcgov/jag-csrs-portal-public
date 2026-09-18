2fd9e126d790b394c159c66d0c51e50377de4ad0
Vlad Belukha <vlad.belukha@quartech.com>
2026-09-18 12:05:38 -0700

    Refactor Dynamics authentication configuration to return native resource name alongside API endpoint URL

--- src/backend/Csrs.Api/Authentication/OAuthError.cs
+++ src/backend/Csrs.Api/Authentication/OAuthError.cs
@@ -4,6 +4,7 @@ namespace Csrs.Api.Authentication
 {
     public class OAuthError
     {
+        [JsonPropertyName("error")]
         public string? Code { get; set; }
         [JsonPropertyName("error_description")]
         public string? Description { get; set; }
